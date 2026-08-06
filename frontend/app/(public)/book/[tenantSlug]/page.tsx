a98e15cb569210f30f21746b970583e062cbbb82
Ömer Faruk Uysal <ouysal155@gmail.com>
2026-08-06 22:50:21 +0300

    fix(build): fix import syntax in book page

--- frontend/app/(public)/book/[tenantSlug]/page.tsx
+++ frontend/app/(public)/book/[tenantSlug]/page.tsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from "react";
 import { useParams, useRouter } from "next/navigation";
 import Link from "next/link";
-import { apiRequest } = from "@/lib/api-client";
+import { apiRequest } from "@/lib/api-client";
 
 interface ServiceItem {
   id: string;
